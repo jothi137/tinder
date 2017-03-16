f96a6b9177c9266190b921fc67d0e09446fb12e9
Jothi Venkataraman <jvenkataraman@splunk.com>
2017-03-16 14:12:48 -0700

    Booklist conatiner

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+    return [
+        {title: 'JavaScript: TheGood Parts'},
+        {title: 'Harry Potter and the Prisoner of Azkaban'},
+        {title: 'Da Vinci Code'},
+        {title: 'Immortals of Meluha'}
+    ]
+}
